a0695d93ad5a776b4c6a377a6101b5dab9f7012e
NachoFD <110072609+NachoFD@users.noreply.github.com>
2022-09-12 20:38:10 -0300

    Update RendicionHoras(PARCIAL).sql

--- DDL/RendicionHoras(PARCIAL).sql
+++ DDL/RendicionHoras(PARCIAL).sql
@@ -1,10 +1,12 @@
 create table HorasCargadas (
 idHC int not null auto_increment,
+idProyecto int not null,
 Legajo int not null,
 idRol int not null,
 Horas int,
 
 Primary Key (idHC),
+foreign key (idProyecto) references proyecto(id),
 foreign key (Legajo) references persona(Legajo),
 foreign key (idRol) references persona(idRol)
 );
@@ -18,7 +20,7 @@ begin
 end //
 
 
-insert into horascargadas (idHC, Legajo, idRol, Horas)
-values (1, 1111, 1, 0), (2,2222,2,0), (3,3333,3,0);
+insert into horascargadas (idHC, idProyecto, Legajo, idRol, Horas)
+values (1, 1, 1111, 1, 0), (2, 1, 2222,2,0), (3, 1, 3333,3,0);
 
 call RendicionHoras (1111, 3);
