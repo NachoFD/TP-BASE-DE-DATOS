6ead2919415cfaea7e1aa057e7ac98ffd9120d9e
NachoFD <110072609+NachoFD@users.noreply.github.com>
2022-09-12 20:43:37 -0300

    Update RendicionHoras(PARCIAL).sql

--- DDL/RendicionHoras(PARCIAL).sql
+++ DDL/RendicionHoras(PARCIAL).sql
@@ -1,3 +1,6 @@
+alter table persona
+add idProyecto int;
+
 create table HorasCargadas (
 idHC int not null auto_increment,
 idProyecto int not null,
@@ -12,15 +15,15 @@ foreign key (idRol) references persona(idRol)
 );
 
 delimiter //
-create procedure RendicionHoras(numLegajo int, hora int)
+create procedure RendicionHoras(numLegajo int, numP int, hora int)
 begin 
     update horascargadas
     set Horas = Horas + hora
-    where Legajo = numLegajo;
+       where Legajo = numLegajo and idProyecto = numP;
 end //
 
 
 insert into horascargadas (idHC, idProyecto, Legajo, idRol, Horas)
 values (1, 1, 1111, 1, 0), (2, 1, 2222,2,0), (3, 1, 3333,3,0);
 
-call RendicionHoras (1111, 3);
+call RendicionHoras (1111, 1, 3);
